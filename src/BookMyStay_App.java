c04380c75c30cae8f91d575f1a2dcab05ff4a077
bharat-kurmadasu <kbkkbk600@gmail.com>
2026-03-12 15:32:20 +0530

    UC5: room searh

---
+++ src/BookMyStay_App.java
@@ -0,0 +1,22 @@
+public class BookMyStay_App {
+    public static void main(String[] args) {
+
+        System.out.println("Booking Request Queue");
+        BookingRequestQueue bookingQueue = new BookingRequestQueue();
+        Reservation r1 = new Reservation("Abhi", "Single");
+        Reservation r2 = new Reservation("Subha", "Double");
+        Reservation r3 = new Reservation("Vanmathi", "Suite");
+        bookingQueue.addRequest(r1);
+        bookingQueue.addRequest(r2);
+        bookingQueue.addRequest(r3);
+        while (bookingQueue.hasPendingRequests()) {
+            Reservation request = bookingQueue.getNextRequest();
+            System.out.println(
+                    "Processing booking for Guest: " +
+                            request.getGuestName() +
+                            ", Room Type: " +
+                            request.getRoomType()
+            );
+        }
+    }
+}
